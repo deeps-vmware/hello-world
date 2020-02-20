b970a448961f81755149763314259ac60e227c56
deeps-vmware <deeps@vmware.com>
2020-02-19 21:31:25 -0500

    added UPSTREAM support

--- README.md
+++ README.md
@@ -3,24 +3,37 @@
 Simple http server responding "Hello World! from {NODE_ID}:{PORT}"
 
     > docker run --rm -p 8090:8090 deepsvmwarecom/hello-world
-    Hello World! from 172.17.0.2:8090
+    Hello World! from d2e537fa4cd0 172.17.0.2:8090
 
 Now you can hit the sever on localhost
 
     > curl localhost:8090
-    Hello World! from 172.17.0.2:8090 (1)
+    Hello World! from d2e537fa4cd0:8090 (1)
 
 ## defaults
 
 | ENV       | default value |
 |-----------|---------------|
 | PORT      | 8090          |
-| NODE_ID   | public ip     |
+| NODE_ID   | hostname      |
+| UPSTREAM  | nil           |
 
 You can override these as follows
 
-    > docker run --rm -p 8080:8888 -e PORT=8888 -e NODE_ID=my-server-01 deepsvmwarecom/hello-world
-    Hello World! from my-server-01:8888
+    > docker run --rm -p 8090:8888 -e PORT=8888 -e NODE_ID=my-server-01 deepsvmwarecom/hello-world
+    Hello World! from my-server-01 172.17.0.2:8888
 
-    > curl localhost:8080
+    > curl localhost:8090
     Hello World! from my-server-01:8888 (1)
+
+## upstream
+
+An upstream http GET call is made if env `UPSTREAM` is provided
+
+    > docker run --rm -p 8091:8888 -e PORT=8888 -e NODE_ID=my-server-02 -e UPSTREAM=http://172.17.0.2:8888 deepsvmwarecom/hello-world
+    Hello World! from my-server-02 172.17.0.3:8888
+    Upstream: http://172.17.0.2:8888
+
+    > curl localhost:8091
+    Hello World! from my-server-02:8888 (1)
+    Upstream: Hello World! from my-server-01:8888 (2)
--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"os"
 	"net"
 	"net/http"
+	"io/ioutil"
 )
 
 var count = 0
@@ -12,7 +13,22 @@ var count = 0
 func index(w http.ResponseWriter, req *http.Request) {
 	count = count + 1
 	fmt.Fprintf(w, "Hello World! from %s:%s (%d)\n", os.Getenv("NODE_ID"), os.Getenv("PORT"), count)
+	getUpstream(w)
 }
+
+func getUpstream(w http.ResponseWriter) {
+	upstream := os.Getenv("UPSTREAM")
+	if upstream != "" {
+		resp, err := http.Get(upstream)
+		if err != nil {
+			// handle error
+		}
+		defer resp.Body.Close()
+		body, _ := ioutil.ReadAll(resp.Body)
+		fmt.Fprintf(w, "Upstream: %s", string(body))
+	}
+}
+
 
 func main() {
 	http.HandleFunc("/", index)
@@ -22,14 +38,21 @@ func main() {
 		os.Setenv("PORT", port)
 	}
 
-	nodeIP := os.Getenv("NODE_ID")
-	if nodeIP == "" {
-		conn, _ := net.Dial("udp", "8.8.8.8:80")
-		defer conn.Close()
-		localAddr := conn.LocalAddr().(*net.UDPAddr)
-		nodeIP = localAddr.IP.String()
-		os.Setenv("NODE_ID", nodeIP)
+	conn, _ := net.Dial("udp", "8.8.8.8:80")
+	defer conn.Close()
+	localAddr := conn.LocalAddr().(*net.UDPAddr)
+
+	nodeID := os.Getenv("NODE_ID")
+	if nodeID == "" {
+		nodeID, _ = os.Hostname()
+		os.Setenv("NODE_ID", nodeID)
 	}
-	fmt.Printf("Hello World! from %s:%s\n", os.Getenv("NODE_ID"), os.Getenv("PORT"))
-	http.ListenAndServe(":"+port, nil)
+	fmt.Printf("Hello World! from %s:%s\n", nodeID + " " + localAddr.IP.String(), os.Getenv("PORT"))
+	if os.Getenv("UPSTREAM") != "" {
+		fmt.Printf("Upstream: %s\n", os.Getenv("UPSTREAM"))
+	}
+	err := http.ListenAndServe(":"+port, nil)
+	if err != nil {
+		panic(err)
+	}
 }
